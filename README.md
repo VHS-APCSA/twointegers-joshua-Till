b28fddeb6b8f92a9f5f23a738dab4b7e8a2a7967
bob <bob@APCSA>
2018-09-14 15:35:07 -0700

    worked all the way up to boolean

--- README.md
+++ README.md
@@ -8,10 +8,13 @@ For problem 2.15 write a method named arithmetic() and use the two instance vari
 to calculate the values of sum, product, difference and quotient.
 The method should return a String that displays the results.
 Something like "5 + 3 = 8, 5 * 3 = 15, 5 / 3 = 1, 5 % 3 = 2" with 5 and 3 replaced with your actual numbers.
+
 For problem 2.16 write a method named larger() that returns the larger integer. 
 If they're equal than it doesn't matter which one you return.
+
 For problem 2.25 write a method called isEven() that returns a boolean if the sum of the 
 two integers is even and false if the sum is odd.
+
 For problem 2.26 return a boolean that is true if the first int is a multiple of the 
 second int and false otherwise.
 
---
+++ src/TwoIntegers.java
@@ -0,0 +1,62 @@
+
+public class TwoIntegers 
+{
+	private int num1;
+	private int num2;
+	
+	public TwoIntegers()
+	{
+		num1 = 0;
+		num2 = 0;
+	}
+	public TwoIntegers(int num1, int num2)
+	{
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+	public int getNum1()
+	{
+		return num1;
+	}
+	public void setNum1(int num1)
+	{
+		this.num1 = num1;
+	}
+	public int getNum2()
+	{
+	return num2;
+	}
+	public void setNum2(int num2)
+	{
+		this.num2 = num2;
+	}
+	public String arithmetic()
+	{
+		int sum = this.num1 + this.num2;
+		int product = this.num1 * this.num2;
+		int difference = this.num1 - this.num2;
+		int quotient = this.num1 / this.num2;
+		return num1 + " + " + num2 + "  = " + sum
+		+ " // " + num1 + " * " + num2 + " = " + product + " // " +
+		num1 + " - " + num2 + " = " + difference + " // " +
+		num1 + " / " + num2 + " = " + quotient + " // ";
+	}
+	public int larger()
+	{
+		if(num1 > num2) 
+		{
+			return num1;
+		}
+		else if(num1 < num2)
+		{
+			return num2;
+		}
+		else
+		{
+			return num1;
+		}
+	}
+	
+}
+
+
